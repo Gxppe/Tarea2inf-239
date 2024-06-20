fab05f9e77c6b10aa1a7ccc04aa73ee8f5ba6956
Sofiwiwi <sofia.ramirezc@usm.cl>
2024-06-20 15:27:24 -0400

    funcion main en cliente.py

--- Client/cliente.py
+++ Client/cliente.py
@@ -1,9 +1,8 @@
 import requests
 global correo_ingresado, clave_ingresada
 
-
-
-
+#####################################################################################################################
+# Menús
 
 menu_inicial = """
 ------------------------------------
@@ -16,21 +15,25 @@ Elige una de las siguientes opciones:
 
 """
 menu_opciones = """
-------------------------------------
-|¡Hola de nuevo! ¿Qué deseas hacer?|
-------------------------------------
+------------------------------------------------
+|⋆˚☆˖°¡Hola de nuevo! ¿Qué deseas hacer? °˖☆˚⋆|
+------------------------------------------------
 Elige una de las siguientes opciones:
 1. Ver informacion sobre un correo electronico
 2. Ver correos marcados como favoritos
 3. Marcar correo como favorito
-4. Salir
+4. Desmarcar correo como favorito
+5. Bloquear usuario
+6. Salir
 
 """
 menu_salida = """
---------------------------------------
-|๋࣭ ⭑⚝¡Gracias por usar CommuniKen!⚝⭑ ࣭ ๋|
---------------------------------------
+---------------------------------------
+| ๋࣭ ⭑⚝¡Gracias por usar CommuniKen!⚝⭑ ࣭ ๋|
+---------------------------------------
 """
+#####################################################################################################################
+# Funciones de opciones
 def opcion_inicial():
     print(menu_inicial)
     opcion = int(input("Ingrese el número de la opción que desea: "))
@@ -40,24 +43,25 @@ def opcion_menu():
     print(menu_opciones)
     opcion = int(input("Ingrese el número de la opción que desea: "))
     return opcion
-
+#####################################################################################################################
+# Funciones
 def registro():
     print("* Obligatorio")
     nombre_usuario = input("*Ingrese su nombre de usuario: ")
-    correo = input("*Ingrese su correo electronico: ")
-    clave = input("*Ingrese su clave: ")
+    correo_ingresado = input("*Ingrese su correo electronico: ")
+    clave_ingresada = input("*Ingrese su clave: ")
     descripcion = input("Ingrese una descripción: ")
-    if not nombre or not correo or not clave:
+    if not nombre or not correo_ingresado or not clave_ingresada:
         print("Por favor, llene los campos obligatorios")
         registro()
     url = 'http://localhost:3000/api/registrar'
     data = {
         'nombre_usuario': nombre_usuario,
-        'correo': correo,
-        'clave': clave,
+        'correo': correo_ingresado,
+        'clave': clave_ingresada,
         'descripcion': descripcion
     }
-    response = requests.post(url, data=data)
+    response = requests.post(url, params=data)
     print(response.json())
 
 def iniciar_sesion():
@@ -69,15 +73,85 @@ def iniciar_sesion():
 def ver_informacion():
     correo = input("Ingrese el correo electronico que desea buscar: ")
     if not correo:
-        print("No ingresaste nada :c")
-        return
+        print("No ingresaste nada, intenta de nuevo.")
+        ver_informacion()
     url = 'http://localhost:3000/api/informacion'
     data = {
         'correo': correo
     }
+    response = requests.get(url, params=data)
+    print(response.json())
+
+def marcar():
+    id_correo = int(input("Ingrese el id del correo que desea marcar como favorito: "))
+    url = 'http://localhost:3000/api/marcarcorreo'
+    data = {
+        'correo': correo_ingresado
+        'clave': clave_ingresada
+        'id_correo': id_correo
+    }
+    response = requests.post(url, params=data)
+    print(response.json())
+
+def bloquear():
+    correo_bloqueado = input("Ingrese el correo que desea bloquear: ")
+    url = 'http://localhost:3000/api/bloquear'
+    data = {
+        'correo': correo_ingresado
+        'clave': clave_ingresada
+        'correo_bloqueado': correo_bloqueado
+    }
+    response = requests.post(url, params=data)
+    print(response.json())
 
 
 
+
+#####################################################################################################################
+# Main
+def main():
+    opcioni = opcion_inicial()
+    if opcioni<=3 and opcioni>=1: 
+        if opcioni == 1:
+            registro()
+            if response.status_code == 200:
+                print("Registro exitoso")
+            else:
+                print("Registro fallido.")
+                opcioni = 3
+        if opcioni == 2:
+            iniciar_sesion()
+            if response.status_code == 200:
+                print("Inicio de sesión exitoso")
+            else:
+                print("Inicio de sesión fallido.")
+                opcioni = 3
+        if opcioni == 3:
+            print(menu_salida)
+            return
+    else:
+        print("Opción no válida, intenta de nuevo.")
+        main()
+
+    while True:
+        opcion = opcion_menu()
+        if opcion == 1:
+            ver_informacion()
+        elif opcion == 2:
+            print("Ver correos marcados como favoritos")
+        elif opcion == 3:
+            marcar()
+        elif opcion == 4:
+            print("Desmarcar correo como favorito")
+        elif opcion == 5:
+            bloquear()
+        elif opcion == 6:
+            print(menu_salida)
+            break
+        else:
+            print("Opción no válida, intenta de nuevo.")
+            continue
+    return
 
 
 response = requests.get('https://api.github.com')
